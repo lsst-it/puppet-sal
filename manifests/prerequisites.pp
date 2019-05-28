7885f202bd87cf40d2bd65a1aedc25d6894a3c56
Bill Glick <bglick@illinois.edu>
2019-05-28 15:15:35 -0500

    Change fitsio file to only use verion number

--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -13,7 +13,7 @@ class sal::prerequisites (
 
 
   $fits_tarball = basename($fitsio_tar_url)
-  $fits_name = regsubst(basename($fitsio_tar_url, '.tar.gz'), '\w+', '')
+  $fits_name = regsubst(basename($fitsio_tar_url, '.tar.gz'), '\w+', '\\1')
   exec { "download_fitsio":
     path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
 #    unless  => "find /root -type d | grep -i $fits_name",
